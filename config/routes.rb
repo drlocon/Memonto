d2ff294d063c28968ada80f95982c6c619a42e8d
drlocon <prirautem.srw.09@gmail.com>
2022-07-08 08:49:25 +0000

    [Add]ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,38 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'documents/new'
-    get 'documents/index'
-    get 'documents/show'
-    get 'documents/edit'
-    get 'documents/word_search'
-    get 'documents/tag_search'
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  devise_for :end_users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  scope module: :public do
+    root to: 'homes#top'
+    
+    resource :end_users, only:[:show, :edit, :update] do
+      get "confirm" => "end_users#confirm"
+      patch "withdrawal" => "end_users#withdrawal"
+    end
+    
+    resources :documents do
+      collection do
+        get "word_search" => "documents#word_search"
+        get "tag_search" => "documents#tag_search"
+      end
+    end
   end
-  namespace :public do
-    get 'end_users/show'
-    get 'end_users/edit'
-    get 'end_users/confirm'
-  end
+  
   namespace :admin do
-    get 'end_users/show'
-    get 'end_users/edit'
-    get 'end_users/word_search'
+    get "/" => "home#top"
+    
+    resource :end_users, only:[:show, :edit, :update] do
+      collection do
+        get "word_search" => "end_users#word_search"
+      end
+    end
   end
-  namespace :public do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  devise_for :admins
-  devise_for :end_users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
