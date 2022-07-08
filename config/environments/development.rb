b047c0851a42c84a3e1b88d1149d952d42c0024e
drlocon <prirautem.srw.09@gmail.com>
2022-07-08 09:23:00 +0000

    [Add]ビューを追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "8f4232ee0d8b496cb58c2f2332fdf26c.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
