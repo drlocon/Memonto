c39579d69eba9c4711732b45dff8fbfa6dede211
drlocon <prirautem.srw.09@gmail.com>
2022-07-09 06:16:11 +0000

    [Add]記録内容の新規登録を実装

--- app/controllers/public/documents_controller.rb
+++ app/controllers/public/documents_controller.rb
@@ -1,5 +1,20 @@
 class Public::DocumentsController < ApplicationController
+  before_action :authenticate_end_user!
+  
   def new
+    @document = Document.new
+  end
+  
+  def create
+    document = Document.new(document_params)
+    if document.save
+      flash[:notice] = '記録を登録しました'
+      redirect_to documents_path
+    else
+      @document = Document.new
+      flash[:alert] = '入力に誤りがあります'
+      render :new
+    end
   end
 
   def index
@@ -15,5 +30,11 @@ class Public::DocumentsController < ApplicationController
   end
 
   def tag_search
+  end
+  
+  private
+  
+  def document_params
+    params.require(:document).permit(:content, :feeling, :name)
   end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -11,7 +11,7 @@
 #
 class Document < ApplicationRecord
   belongs_to :end_users
-  
+
   has_many :tag_documents, dependent: :destroy
   has_many :favorites, dependent: :destroy
 end
