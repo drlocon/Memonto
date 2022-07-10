eb48667739b3824ac73ec8b4d0c2489199d10ab8
drlocon <prirautem.srw.09@gmail.com>
2022-07-10 10:46:52 +0000

    [Fix]不具合を修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,21 +1,16 @@
 class Public::FavoritesController < ApplicationController
-  before_action :set_favorite, only: [:create, :destroy]
-  
+
   def create
+    document = Document.find(params[:document_id])
     @favorite = current_end_user.favorites.new(document_id: document.id)
     @favorite.save
     render 'replace_btn'
   end
   
   def destroy
+    document = Document.find(params[:document_id])
     @favorite = current_end_user.favorites.find_by(document_id: document.id)
     @favorite.destroy
     render 'replace_btn'
-  end
-  
-  private
-  
-  def set_favorite
-    @document = Document.find(params[:document_id])
   end
 end
