e7936b624850b61b433489aef329be16bdb55b89
drlocon <prirautem.srw.09@gmail.com>
2022-07-11 18:13:57 +0000

    [Add]編集、更新機能を追加

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,13 +1,33 @@
 class Admin::EndUsersController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_end_user, only: [:show, :edit, :update]
   
   def show
-    @end_user = EndUser.find(params[:id])
   end
 
   def edit
   end
+  
+  def update
+    if @end_user.update(end_user_params)
+      flash[:notice] = "ユーザー情報が更新されました"
+      redirect_to admin_end_user_path(@end_user)
+    else
+      flash[:alert] = "ユーザー情報を更新できませんできた"
+      render :edit
+    end
+  end
 
   def word_search
+  end
+  
+  private
+  
+  def end_user_params
+    params.require(:end_user).permit(:name, :email, :is_deleted)
+  end
+  
+  def set_end_user
+    @end_user = EndUser.find(params[:id])
   end
 end
