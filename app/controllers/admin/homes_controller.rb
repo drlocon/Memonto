3af378d4d92e9be3c4167f072194eecf0541c393
drlocon <prirautem.srw.09@gmail.com>
2022-07-15 03:51:01 +0000

    [Update]管理者トップを変更

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,6 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   
   def top
-    @end_users = EndUser.all
+    @end_users = EndUser.all.page(params[:page]).per(8)
   end
 end
