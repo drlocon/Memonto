3db2ca6e10ae383eeec6a8bf664aa30fa52cc96d
drlocon <prirautem.srw.09@gmail.com>
2022-07-26 13:10:24 +0000

    [Fix]テストデータの不具合を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Admin.create!(
   password: 'memonto'
 )
 
-# テストデータ
+# ユーザアカウント(12名)
 users = EndUser.create!(
   [
     {email: 'daisy@test.com',    name: 'Daisy',     password: 'dai4y8e', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.png"), filename: "sample-user1.png")},
@@ -30,6 +30,7 @@ users = EndUser.create!(
   ]
 )
 
+# ユーザ:Daisy、Tubakiのテストデータ(記録管理機能)
 documents = Document.create!(
   [
     {content: '町の灯は、暗の中だってこわくない', feeling: 'happy',  end_user_id: users[0].id},
@@ -47,10 +48,10 @@ documents = Document.create!(
     {content: '向こうからぱっと白く明るくなった', feeling: 'tired',  end_user_id: users[1].id},
     {content: '地平線の上でけむったように思った', feeling: 'sorrow', end_user_id: users[1].id},
     {content: '幸福なそのひとのために祈っている', feeling: 'normal', end_user_id: users[1].id}
-    
   ]
 )
 
+# ユーザ:Daisy、Tubakiのテストデータ(タグ機能)
 tags = Tag.create!(
   [
     {name: 'ダミー'},
@@ -76,7 +77,6 @@ TagDocument.create!(
     {tag_id: tags[0].id, document_id: documents[11].id},
     {tag_id: tags[0].id, document_id: documents[12].id},
     {tag_id: tags[1].id, document_id: documents[13].id},
-    {tag_id: tags[2].id, document_id: documents[14].id},
-    {tag_id: tags[3].id, document_id: documents[15].id}
+    {tag_id: tags[2].id, document_id: documents[14].id}
   ]
 )
